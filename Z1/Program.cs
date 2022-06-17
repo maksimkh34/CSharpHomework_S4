bc15eb2f3297db0c3a6ca241a2e7bb38c4844ad2
Maksim <epic.twod@mail.ru>
2022-06-17 10:19:48 +0300

    Task 1

---
+++ Z1/Program.cs
@@ -0,0 +1,19 @@
+﻿int power(int n, int m)
+{
+    int result = n;
+    while(m>1)
+    {
+        result = result * n;
+        m--;
+    }
+    return result;
+}
+
+Console.Write("Enter A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Answer is: " + power(a, b));
+//Console.Write("Answer is: " + Math.Pow(a, b));
